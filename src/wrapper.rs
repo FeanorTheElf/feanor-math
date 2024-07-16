1b0a8e5fc13f4eea20cc9d4594f05f86a4ae6aaa
Simon Pohmann <simon@pohmann.de>
2024-07-16 12:31:47 +0100

    Added ref + ref functions for RingElementWrapper

--- src/wrapper.rs
+++ src/wrapper.rs
@@ -83,7 +83,7 @@ macro_rules! impl_xassign_trait {
 }
 
 macro_rules! impl_trait {
-    ($trait_name:ident, $fn_name:ident) => {
+    ($trait_name:ident, $fn_name:ident, $fn_name_ref_fst:ident, $fn_name_ref_snd:ident, $fn_name_ref:ident) => {
         
         impl<R: RingStore> $trait_name for RingElementWrapper<R> {
             type Output = Self;
@@ -93,15 +93,42 @@ macro_rules! impl_trait {
                 Self { ring: self.ring, element: rhs.ring.$fn_name(self.element, rhs.element) }
             }
         }
+
+        impl<'a, R: RingStore> $trait_name<RingElementWrapper<R>> for &'a RingElementWrapper<R> {
+            type Output = RingElementWrapper<R>;
+
+            fn $fn_name(self, rhs: RingElementWrapper<R>) -> Self::Output {
+                debug_assert!(self.ring.get_ring() == rhs.ring.get_ring());
+                RingElementWrapper { ring: rhs.ring, element: self.ring.$fn_name_ref_fst(&self.element, rhs.element) }
+            }
+        }
+
+        impl<'a, R: RingStore> $trait_name<&'a RingElementWrapper<R>> for RingElementWrapper<R> {
+            type Output = RingElementWrapper<R>;
+
+            fn $fn_name(self, rhs: &'a RingElementWrapper<R>) -> Self::Output {
+                debug_assert!(self.ring.get_ring() == rhs.ring.get_ring());
+                RingElementWrapper { ring: self.ring, element: rhs.ring.$fn_name_ref_snd(self.element, &rhs.element) }
+            }
+        }
+
+        impl<'a, 'b, R: RingStore + Clone> $trait_name<&'a RingElementWrapper<R>> for &'b RingElementWrapper<R> {
+            type Output = RingElementWrapper<R>;
+
+            fn $fn_name(self, rhs: &'a RingElementWrapper<R>) -> Self::Output {
+                debug_assert!(self.ring.get_ring() == rhs.ring.get_ring());
+                RingElementWrapper { ring: self.ring.clone(), element: self.ring.$fn_name_ref(&self.element, &rhs.element) }
+            }
+        }
     };
 }
 
 impl_xassign_trait!{ AddAssign, add_assign, add_assign_ref }
 impl_xassign_trait!{ MulAssign, mul_assign, mul_assign_ref }
 impl_xassign_trait!{ SubAssign, sub_assign, sub_assign_ref }
-impl_trait!{ Add, add }
-impl_trait!{ Mul, mul }
-impl_trait!{ Sub, sub }
+impl_trait!{ Add, add, add_ref_fst, add_ref_snd, add_ref }
+impl_trait!{ Mul, mul, mul_ref_fst, mul_ref_snd, mul_ref }
+impl_trait!{ Sub, sub, sub_ref_fst, sub_ref_snd, sub_ref }
 
 macro_rules! impl_xassign_trait_int {
     ($trait_name:ident, $fn_name:ident) => {
@@ -131,6 +158,22 @@ macro_rules! impl_trait_int {
 
             fn $fn_name(self, rhs: RingElementWrapper<R>) -> Self::Output {
                 RingElementWrapper { element: rhs.ring.$fn_name(rhs.element, rhs.ring.int_hom().map(self)), ring: rhs.ring }
+            }
+        }
+
+        impl<'a, R: RingStore + Clone> $trait_name<i32> for &'a RingElementWrapper<R> {
+            type Output = RingElementWrapper<R>;
+
+            fn $fn_name(self, rhs: i32) -> Self::Output {
+                RingElementWrapper { element: self.ring.$fn_name(self.ring.clone_el(&self.element), self.ring.int_hom().map(rhs)), ring: self.ring.clone() }
+            }
+        }
+
+        impl<'a, R: RingStore + Clone> $trait_name<&'a RingElementWrapper<R>> for i32 {
+            type Output = RingElementWrapper<R>;
+
+            fn $fn_name(self, rhs: &'a RingElementWrapper<R>) -> Self::Output {
+                RingElementWrapper { element: rhs.ring.$fn_name(rhs.ring.clone_el(&rhs.element), rhs.ring.int_hom().map(self)), ring: rhs.ring.clone() }
             }
         }
     };
@@ -257,6 +300,40 @@ fn test_arithmetic_expression_int() {
                 let y = RingElementWrapper::new(&ring, y);
                 let z = RingElementWrapper::new(&ring, z);
                 assert_el_eq!(ring, expected, (x * 8 * y + (1 + x + 2 * z) * (y - z * 2) + 5).unwrap());
+            }
+        }
+    }
+}
+
+#[test]
+fn test_arithmetic_expression_ref() {
+    let ring = zn_64::Zn::new(17);
+
+    for x in ring.elements() {
+        for y in ring.elements() {
+            for z in ring.elements() {
+                let expected = ring.add(ring.mul(x, y), ring.mul(ring.add(x, z), ring.sub(y, z)));
+                let x = RingElementWrapper::new(&ring, x);
+                let y = RingElementWrapper::new(&ring, y);
+                let z = RingElementWrapper::new(&ring, z);
+                assert_el_eq!(ring, expected, (x * &y + (&x + &z) * (&y - z)).unwrap());
+            }
+        }
+    }
+}
+
+#[test]
+fn test_arithmetic_expression_int_ref() {
+    let ring = zn_64::Zn::new(17);
+
+    for x in ring.elements() {
+        for y in ring.elements() {
+            for z in ring.elements() {
+                let expected = ring.add(ring.add(ring.int_hom().mul_map(ring.mul(x, y), 8), ring.mul(ring.add(ring.add(ring.one(), x), ring.int_hom().mul_map(z, 2)), ring.sub(y, ring.int_hom().mul_map(z, 2)))), ring.int_hom().map(5));
+                let x = RingElementWrapper::new(&ring, x);
+                let y = RingElementWrapper::new(&ring, y);
+                let z = RingElementWrapper::new(&ring, z);
+                assert_el_eq!(ring, expected, (x * 8 * &y + (1 + &x + 2 * &z) * (&y - z * 2) + 5).unwrap());
             }
         }
     }
