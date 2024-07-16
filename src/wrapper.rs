8266b4a1480340595b044334fe4ec05734ee34cb
Simon Pohmann <simon@pohmann.de>
2024-07-16 12:19:33 +0100

    Added integer operations to RingElementWrapper

--- src/wrapper.rs
+++ src/wrapper.rs
@@ -1,5 +1,6 @@
 use std::{ops::*, hash::Hash, fmt::{Display, Debug}};
 
+use crate::homomorphism::*;
 use crate::ring::*;
 
 ///
@@ -102,6 +103,46 @@ impl_trait!{ Add, add }
 impl_trait!{ Mul, mul }
 impl_trait!{ Sub, sub }
 
+macro_rules! impl_xassign_trait_int {
+    ($trait_name:ident, $fn_name:ident) => {
+        
+        impl<R: RingStore> $trait_name<i32> for RingElementWrapper<R> {
+
+            fn $fn_name(&mut self, rhs: i32) {
+                self.ring.$fn_name(&mut self.element, self.ring.int_hom().map(rhs));
+            }
+        }
+    };
+}
+
+macro_rules! impl_trait_int {
+    ($trait_name:ident, $fn_name:ident) => {
+        
+        impl<R: RingStore> $trait_name<i32> for RingElementWrapper<R> {
+            type Output = Self;
+
+            fn $fn_name(self, rhs: i32) -> Self::Output {
+                RingElementWrapper { element: self.ring.$fn_name(self.element, self.ring.int_hom().map(rhs)), ring: self.ring }
+            }
+        }
+
+        impl<R: RingStore> $trait_name<RingElementWrapper<R>> for i32 {
+            type Output = RingElementWrapper<R>;
+
+            fn $fn_name(self, rhs: RingElementWrapper<R>) -> Self::Output {
+                RingElementWrapper { element: rhs.ring.$fn_name(rhs.element, rhs.ring.int_hom().map(self)), ring: rhs.ring }
+            }
+        }
+    };
+}
+
+impl_xassign_trait_int!{ AddAssign, add_assign }
+impl_xassign_trait_int!{ MulAssign, mul_assign }
+impl_xassign_trait_int!{ SubAssign, sub_assign }
+impl_trait_int!{ Add, add }
+impl_trait_int!{ Mul, mul }
+impl_trait_int!{ Sub, sub }
+
 #[cfg(feature = "elementwrapper-caret-pow")]
 impl<R: RingStore> BitXor<usize> for RingElementWrapper<R> {
     type Output = RingElementWrapper<R>;
@@ -174,6 +215,10 @@ impl<R: RingStore> Deref for RingElementWrapper<R> {
 #[cfg(feature = "elementwrapper-caret-pow")]
 #[cfg(test)]
 use crate::primitive_int::StaticRing;
+#[cfg(test)]
+use crate::rings::finite::FiniteRingStore;
+#[cfg(test)]
+use crate::rings::zn::zn_64;
 
 #[cfg(feature = "elementwrapper-caret-pow")]
 #[test]
@@ -181,4 +226,38 @@ fn test_pow_caret() {
     let ring = StaticRing::<i64>::RING;
     let a = RingElementWrapper::new(&ring, 3);
     assert_eq!(a * a * a * a, a^4);
+}
+
+#[test]
+fn test_arithmetic_expression() {
+    let ring = zn_64::Zn::new(17);
+
+    for x in ring.elements() {
+        for y in ring.elements() {
+            for z in ring.elements() {
+                let expected = ring.add(ring.mul(x, y), ring.mul(ring.add(x, z), ring.sub(y, z)));
+                let x = RingElementWrapper::new(&ring, x);
+                let y = RingElementWrapper::new(&ring, y);
+                let z = RingElementWrapper::new(&ring, z);
+                assert_el_eq!(ring, expected, (x * y + (x + z) * (y - z)).unwrap());
+            }
+        }
+    }
+}
+
+#[test]
+fn test_arithmetic_expression_int() {
+    let ring = zn_64::Zn::new(17);
+
+    for x in ring.elements() {
+        for y in ring.elements() {
+            for z in ring.elements() {
+                let expected = ring.add(ring.add(ring.int_hom().mul_map(ring.mul(x, y), 8), ring.mul(ring.add(ring.add(ring.one(), x), ring.int_hom().mul_map(z, 2)), ring.sub(y, ring.int_hom().mul_map(z, 2)))), ring.int_hom().map(5));
+                let x = RingElementWrapper::new(&ring, x);
+                let y = RingElementWrapper::new(&ring, y);
+                let z = RingElementWrapper::new(&ring, z);
+                assert_el_eq!(ring, expected, (x * 8 * y + (1 + x + 2 * z) * (y - z * 2) + 5).unwrap());
+            }
+        }
+    }
 }
